deaacbf8c98b5003bc17265b01c19eb0156866a1
Spencer Julian <helloThere@spencerjulian.com>
2012-03-10 15:59:36 -0500

    Forgot to add xbmcvc.h. Causes compiling to fail. My apologies to all who attempted to download.

--- parseConfig.h
+++ parseConfig.h
@@ -8,18 +8,24 @@ typedef char* string; //I'm lazy.
 void parseFile(string filename, string* section, string* vocab, string* command)
 {
 	FILE *config;
-	string line;
+	string line, test;
 	int i=0, linenum=0;
 
 	config=fopen(filename, "r");
 	if (config != NULL) {
 		while(fgets(line, MAX_LINE_LENGTH, config) != NULL)
 		{
-			string test;
 			linenum++;
 			if (line[0] == '#') continue; //ignore hashs as comments.
 			else if (line[0] == '\n') continue; //ignore blank lines.
 			else {
+				printf("GO!\n");
+				test = strdup(line);
+				printf("DUPPED!\n");
+				printf("%s\n", test);
+				printf("PRINTED!\n");
+				free(test);
+				printf("FREED!\n");
 				i++;
 			}
 		}
---
+++ xbmcvc.h
@@ -0,0 +1,49 @@
+/*********************************************
+* Constants and things
+*********************************************/
+
+
+/* Constants */
+#define VERSION            "0.4"
+#define USAGE_MESSAGE         "\n" \
+               "Usage: xbmcvc [ -H host ] [ -P port ] [ -V ] [ -h ]\n" \
+               "\n" \
+               "    -H hostname  Hostname or IP address of the XBMC instance you want to control (default: localhost)\n" \
+               "    -P port      Port number the XBMC instance you want to control is listening on (default: 8080)\n" \
+               "    -U username  Username for XBMC\n" \
+               "    -p password  Password for XBMC\n" \
+               "    -D device    Name of ALSA device to capture speech from\n" \
+               "    -V           Print version information and exit\n" \
+               "    -h           Print this help message\n" \
+               "\n"
+
+#define MAX_ACTIONS        5
+#define MAX_LISTEN_TIME    10 //In Seconds
+#define JSON_RPC_DEFAULT_HOST    "localhost"
+#define JSON_RPC_DEFAULT_PORT    8080
+#define JSON_RPC_USER_URL        "http://%s:%s@%s:%s/jsonrpc"
+#define JSON_RPC_URL          "http://%s:%s/jsonrpc"
+#define JSON_RPC_POST         "{\"jsonrpc\":\"2.0\",\"method\":\"%s\",\"id\":1}"
+#define JSON_RPC_POST_WITH_PARAMS   "{\"jsonrpc\":\"2.0\",\"method\":\"%s\",\"params\":{%s},\"id\":1}"
+#define EVENTSERVER_URL       "http://%s:%s/xbmcCmds/xbmcHttp?command="
+#define EVENTSERVER_USER_URL  "http://%s:%s@%s:%s/xbmcCmds/xbmcHttp?command="
+
+/* Macros */
+#define DIE(message)       { printf("Fatal error at %s:%d: %s\n", __FILE__, __LINE__, message); exit(1); }
+
+/* structure passed to CURL callback */
+typedef struct {
+   char**   dst;  /* destination buffer */
+   int   dst_s;   /* destination buffer size */
+} curl_userdata_t;
+
+/* Global configuration variables */
+char* config_json_rpc_user;
+char* config_json_rpc_pass;
+char* config_json_rpc_host;
+char* config_json_rpc_port;
+char* config_alsa_device;
+
+/* Exit flag */
+volatile int exit_flag = 0;
+
